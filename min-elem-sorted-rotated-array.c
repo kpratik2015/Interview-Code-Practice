49b26a63c4a458d8766d66377a4c658e4ab3752b
kpratik2015 <contact@pratikkataria.com>
2017-07-03 23:58:09 +0530

    Minimum element in sorted and rotated array in C

---
+++ min-elem-sorted-rotated-array.c
@@ -0,0 +1,46 @@
+// C program to find minimum element in a sorted and rotated array
+#include <stdio.h>
+
+int findMin(int arr[], int low, int high)
+{
+	// This condition is needed to handle the case when array is not
+	// rotated at all
+	if (arr[high] >= arr[low]) return arr[0];
+
+	// If there is only one element left
+	if (high == low) return arr[low];
+
+	// Find mid
+	int mid = low + (high - low)/2; /*(low + high)/2;*/
+
+	// Check if element (mid+1) is minimum element. Consider
+	// the cases like {3, 4, 5, 1, 2}
+	if (mid < high && arr[mid+1] <= arr[mid])
+	return arr[mid+1];
+
+	// Check if mid itself is minimum element
+	if (mid > low && arr[mid] <= arr[mid - 1])
+	return arr[mid];
+
+	// Decide whether we need to go to left half or right half
+	// {3,4,5,6,7,1,2}
+	if (arr[high] >= arr[mid])
+	    return findMin(arr, low, mid-1);
+	return findMin(arr, mid+1, high);
+}
+
+// Driver program to test above functions
+int main()
+{
+    int tests, n,i,j = 0;
+    int ptr[999]={0};
+    
+    scanf("%d",&tests);
+    for(i = 0 ; i < tests ; i++){
+        scanf("%d",&n);
+        for(j = 0 ; j < n ; j++) {
+            scanf("%d",&ptr[j]);
+        }
+        printf("%d\n",findMin(ptr,0,n-1));
+    }
+}
