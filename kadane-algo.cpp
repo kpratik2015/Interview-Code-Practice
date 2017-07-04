0526e1bb2af3970aa001831caf6e2fad0f75bacd
kpratik2015 <contact@pratikkataria.com>
2017-07-04 11:31:02 +0530

    Kadane's Algo - with cout steps

--- kadane-algo.cpp
+++ kadane-algo.cpp
@@ -7,11 +7,16 @@ int maxSubArraySum(int a[], int size)
 {
     int max_so_far = a[0];
     int curr_max = a[0];
-
+    cout<<"Starting up...\nLet: max_so_far = curr_max = a[0] = "<<a[0]<<endl;
+    cout<<"Entering for loop... \n";
     for (int i = 1; i < size; i++)
     {
+        cout<<"curr_max = max( a[i], curr_max + a[i] );\ncurr_max = max( "<<a[i]<<", "<<curr_max<<" + "<<a[i]<<" );"<<endl;
         curr_max = max(a[i], curr_max+a[i]);
+        cout<<"max_so_far = max( max_so_far, curr_max );\nmax_so_far = max( "<<max_so_far<<", "<<curr_max<<" );"<<endl;
         max_so_far = max(max_so_far, curr_max);
+
+        cout<<"--------\ncurr_max = "<<curr_max<<" ;\nmax_so_far = "<<max_so_far<<" ;\n--------\n";
     }
     
     return max_so_far;
@@ -19,7 +24,7 @@ int maxSubArraySum(int a[], int size)
  
 int main()
 {
-    int a[] = {-2, -3, -4, -1, -2, -1, -5, -3};
+    int a[] = {-2, -3, 4, 1, -2, -1, 5, 3};
     int n = sizeof(a)/sizeof(a[0]);
     int max_sum = maxSubArraySum(a, n);
     cout << "Maximum contiguous sum is " << max_sum << endl;
